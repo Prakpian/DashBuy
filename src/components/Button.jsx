9706d952fbc6d21ead3a97c7da50f5e4cdf952cf
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-04-07 14:20:40 +0100

    hover and click effect

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Button({ variant = "primary", btnText }) {
-  const baseClass = "py-2 px-4";
+  const baseClass = "py-2 px-4 hover:brightness-95 active:scale-95";
   const variants = { primary: "bg-red-600 text-white" };
   return (
     <button className={`${baseClass} ${variants[variant]}`}>{btnText}</button>
