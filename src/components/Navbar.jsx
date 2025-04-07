1e2c119a47e4ec9d83c34de701fabc1e7b67098e
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-04-07 12:11:04 +0100

    increase font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { RiAccountCircle2Line, RiShoppingCartLine } from "react-icons/ri";
 
 function Navbar() {
   return (
-    <nav className="flex flex-col h-15 w-full items-center">
+    <nav className="flex flex-col w-full items-center">
       <section className="flex justify-between max-w-[1200px] w-full p-3">
         <p className="font-bold text-2xl">DashBuy</p>
         <div className="flex gap-5 items-center">
@@ -19,15 +19,17 @@ function Navbar() {
           </div>
         </div>
       </section>
-      <section className="w-full flex gap-4 place-content-center bg-neutral-100 py-2">
-        <p className="hover:underline underline-offset-4 decoration-2">All</p>
-        <p className="hover:underline underline-offset-4 decoration-2">
+      <section className="w-full flex gap-4 place-content-center py-2 border-y">
+        <p className="hover:underline underline-offset-4 decoration-2 font-medium">
+          Shop
+        </p>
+        <p className="hover:underline underline-offset-4 decoration-2 font-medium">
           Clothes
         </p>
-        <p className="hover:underline underline-offset-4 decoration-2">
+        <p className="hover:underline underline-offset-4 decoration-2 font-medium">
           Accessories
         </p>
-        <p className="hover:underline underline-offset-4 decoration-2">
+        <p className="hover:underline underline-offset-4 decoration-2 font-medium">
           Electronics
         </p>
       </section>
