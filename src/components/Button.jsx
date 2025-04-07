fcbd5d50b792a38bf04ee61997ce04e5a7ca822b
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-04-07 18:10:18 +0100

    added line button variant

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Button({ variant = "primary", btnText }) {
   const baseClass = "py-2 px-4 hover:brightness-95 active:scale-95";
-  const variants = { primary: "bg-red-600 text-white" };
+  const variants = {
+    primary: "bg-red-600 text-white",
+    line: "border border-2",
+  };
   return (
     <button className={`${baseClass} ${variants[variant]}`}>{btnText}</button>
   );
