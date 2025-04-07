96c887298c5648abd657573d11ed91173c2beb6a
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-04-07 12:03:13 +0100

    shipping detail

---
+++ src/components/ShippingInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ShippingInfo() {
+  return (
+    <section className="flex gap-5 py-2 w-full bg-neutral-100 place-content-center ">
+      <p>Free shipping over £50</p>
+      <p>|</p>
+      <p>Order by 7pm Next Day Delivery (Mon - Fri)</p>
+    </section>
+  );
+}
+
+export default ShippingInfo;
