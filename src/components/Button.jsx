f0d469922aaec3b71f0f02bab14d9742c8dd5cda
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-04-08 16:48:43 +0100

    added onclick prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-function Button({ variant = "primary", btnText }) {
+function Button({ variant = "primary", btnText, btnClick }) {
   const baseClass = "py-2 px-4 hover:brightness-95 active:scale-95";
   const variants = {
     primary: "bg-red-600 text-white",
     line: "border border-2",
   };
   return (
-    <button className={`${baseClass} ${variants[variant]}`}>{btnText}</button>
+    <button className={`${baseClass} ${variants[variant]}`} onClick={btnClick}>
+      {btnText}
+    </button>
   );
 }
 
