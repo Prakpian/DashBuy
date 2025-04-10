c37169d769c3d5abb674335c19f4af05cb383537
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-04-10 14:13:53 +0100

    layout template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
-import "./App.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import ShippingInfo from "./components/ShippingInfo";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ShippingInfo />
+      <Outlet />
+      <Footer />
     </>
   );
 }
