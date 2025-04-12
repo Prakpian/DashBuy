b47f061050d96bbc3b070b7e9d0c6eddbe9fc9c6
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-04-12 01:13:50 +0100

    clickable item and buttn

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
+import { useCart } from "../contexts/CartContext";
 
 function Card({ itemDetails }) {
+  const { setCart } = useCart();
   const navigate = useNavigate();
 
   const handleCardClick = () => {
@@ -11,26 +13,44 @@ function Card({ itemDetails }) {
     });
   };
 
+  const handleAddCartClick = () => {
+    setCart((items) => {
+      if (items.find((item) => item.id === itemDetails.id)) {
+        return items.map((item) =>
+          item.id === itemDetails.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+      return [...items, { ...itemDetails, quantity: 1 }];
+    });
+  };
+
   return (
     <div
       className="flex flex-col gap-2 w-[150px] sm:w-[180px] flex-none"
       key={itemDetails.id}
-      onClick={handleCardClick}
     >
-      <img
-        src={itemDetails.image}
-        alt={itemDetails.title}
-        className="h-[180px] w-[180px] object-contain "
-      />
-      <div>
-        <h3 className="font-medium truncate">{itemDetails.title}</h3>
-        <h2 className="font-light text-neutral-500">
-          {itemDetails.category.charAt(0).toUpperCase() +
-            itemDetails.category.slice(1)}
-        </h2>
+      <div onClick={handleCardClick}>
+        <img
+          src={itemDetails.image}
+          alt={itemDetails.title}
+          className="h-[180px] w-[180px] object-contain "
+        />
+        <div>
+          <h3 className="font-medium truncate">{itemDetails.title}</h3>
+          <h2 className="font-light text-neutral-500">
+            {itemDetails.category.charAt(0).toUpperCase() +
+              itemDetails.category.slice(1)}
+          </h2>
+        </div>
+        <p className="my-2">£{itemDetails.price}</p>
       </div>
-      <p className="my-2">£{itemDetails.price}</p>
-      <Button btnText={"Add to cart"} variant="line" />
+      <Button
+        btnText={"Add to cart"}
+        variant="line"
+        btnClick={handleAddCartClick}
+      />
     </div>
   );
 }
