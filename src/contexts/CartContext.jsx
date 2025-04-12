8dd3cb189ccdfe7499706bb8122b8433803c47e0
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-04-12 01:04:21 +0100

    global access to cart

---
+++ src/contexts/CartContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <CartContext.Provider value={{ cart, setCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) throw new Error("useCart must be used within CartProvider");
+  return context;
+}
