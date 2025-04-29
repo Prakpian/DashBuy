353d6f02c51b8b749d1e32fa0aba91457198d338
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-04-29 23:21:58 +0100

    added ScrollRestoration to scroll to the top

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import ShippingInfo from "./components/ShippingInfo";
 import useFetch from "./hooks/useFetch";
 import { CartProvider } from "./contexts/CartContext";
@@ -12,6 +12,7 @@ function App() {
     <CartProvider>
       <Navbar />
       <ShippingInfo />
+      <ScrollRestoration />
       <Outlet context={{ data, loading }} />
       <Footer />
     </CartProvider>
